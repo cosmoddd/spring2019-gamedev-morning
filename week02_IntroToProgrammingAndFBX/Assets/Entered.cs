ff71224e8750d126bed5b9e478b159909aaa1b08
gregheff@gmail.com <gregheff@gmail.com>
2019-02-07 09:22:59 -0500

    Week 2 - Blender and Control

---
+++ week02_IntroToProgrammingAndFBX/Assets/Entered.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entered : MonoBehaviour
+{
+
+    public GameObject hintText;
+
+    void Start()
+    {
+        if (hintText)
+            {hintText.SetActive(false);}
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        print("Something entered this trigger.  Whoop-de-doo. A "+c.transform.name);
+        hintText.SetActive(true);
+    }
+}
---
+++ week02_IntroToProgrammingAndFBX/Assets/TankControls.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankControls : MonoBehaviour
+{
+    public GameObject movingObject;
+    public float force = 5f;
+    public float torque  = 20f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))         // MOVE FORWARD WHEN YOU HIT "W" KEY
+        {
+            movingObject.transform.Translate(Vector3.forward * Time.deltaTime * force);
+        }
+
+        if (Input.GetKey(KeyCode.S))        // MOVE BACKWARDS WHEN YOU HIT "S" KEY
+        {
+            movingObject.transform.Translate(Vector3.back * Time.deltaTime * force);
+        }
+
+        if (Input.GetKey(KeyCode.A))            // ROTATE LEFT WHEN YOU HIT "A" KEY
+        {
+            movingObject.transform.Rotate(new Vector3 (0,-1,0) * Time.deltaTime * torque);
+        }
+
+        if (Input.GetKey(KeyCode.D))            // ROTATE RIGHT WHEN YOU HIT "D KEY
+        {
+            movingObject.transform.Rotate(new Vector3 (0,1,0) * Time.deltaTime * torque);
+        }
+
+    }
+}
