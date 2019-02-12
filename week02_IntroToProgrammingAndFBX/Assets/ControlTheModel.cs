d66817362e8332d93d55ae05d3787a289c02b439
gregheff@gmail.com <gregheff@gmail.com>
2019-02-12 09:51:09 -0500

    new material

---
+++ week02_IntroToProgrammingAndFBX/Assets/ControlTheModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlTheModel : MonoBehaviour
+{
+
+    public GameObject weirdObject;  // THIS IS THE OBJECT I WANT TO MOVE
+    public float speed;
+    public float torque;
+
+    void Update()       // MY INPUT COMMANDS GO HERE
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            weirdObject.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            weirdObject.transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            weirdObject.transform.Rotate((new Vector3(0,-1,0)) * Time.deltaTime * torque);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            weirdObject.transform.Rotate((new Vector3(0,1,0)) * Time.deltaTime * torque);
+        }
+
+    }
+}
