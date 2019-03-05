f161ff7d9bf8b1688344c881b80ebe8cf6643c25
gregheff@gmail.com <gregheff@gmail.com>
2019-03-04 19:25:59 -0500

    Week 5, revised

---
+++ Week 5 - Raycasting/Assets/Scripts/cmd_DetectGroundFromRay_r2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cmd_DetectGroundFromRay_r2 : MonoBehaviour
+{
+    public float rayDistance = 2;
+
+    void Update()
+    {
+        Ray myRay = new Ray(this.transform.position, Vector3.down);
+
+        Debug.DrawRay(myRay.origin, new Vector3(0, 0, rayDistance), Color.red);
+
+        RaycastHit myHit;
+
+        if (Physics.Raycast(myRay.origin, myRay.direction, out myHit, rayDistance))
+        {
+            print("This roomba hit a "+myHit.rigidbody.transform.name);
+        }
+
+    }    
+
+}
---
+++ Week 5 - Raycasting/Assets/Scripts/cmd_RaycastMouse_r3.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cmd_RaycastMouse_r3 : MonoBehaviour
+{
+    public float cameraRayDistance = 40f;
+
+    public Transform paintCube;
+
+    public bool pinWheelMode;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(mouseRay.origin, (mouseRay.direction * cameraRayDistance), Color.red);
+    
+        RaycastHit myHit;
+
+        if (Physics.Raycast(mouseRay.origin, mouseRay.direction, out myHit, cameraRayDistance))
+        {
+            paintCube.transform.position = myHit.point;
+
+            if (myHit.transform.tag == "Donuts")
+            {
+                myHit.transform.Rotate(0,1,0);
+
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                Transform newCube = Instantiate(paintCube, paintCube.transform.position, Quaternion.Euler(0,0,0));
+
+                if (pinWheelMode)
+                {
+                    newCube.SetParent(myHit.transform, true);
+                }
+            }
+        }
+
+        else
+        {
+            paintCube.transform.position = mouseRay.direction;
+        }
+
+        
+
+    }
+}
