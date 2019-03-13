9d7a2e6afb971821e2f0630ad9ff303aad66b56f
gregheff@gmail.com <gregheff@gmail.com>
2019-03-13 11:37:27 -0400

    Added Week 7 - Audio Implementation

---
+++ Week 7 - Audio/Week 7 - Audio Implementation/Assets/Scripts/cmd_FPSConrol_r2.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cmd_FPSConrol_r2 : MonoBehaviour
+{
+    public Rigidbody thisRigidBody; // the rigidbody we'll be moving
+    public Camera thisCamera;   // the camera
+
+    public float pitch; // the mouse movement up/down
+    public float yaw;   // the mouse movement left/right
+
+    public float fpForwardBackward; // input float from  W and S keys
+    public float fpStrafe;  // input float from A D keys
+
+    public Vector3 inputVelocity;  // cumulative velocity to move character
+
+    public float velocityModifier;  // velocity of conroller multiplied by this number
+    float verticalLook = 0f; 
+
+    void Start()
+    {
+        thisRigidBody = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        yaw = Input.GetAxis("Mouse X");
+        transform.Rotate(0, yaw, 0);
+
+        pitch = Input.GetAxis("Mouse Y");
+
+		// BETTER MOUSE LOOK:
+		// add mouse input to verticalLook, then clamp verticalLook
+        verticalLook += -pitch;
+		verticalLook = Mathf.Clamp(verticalLook, -80f, 80f);
+		
+		// actually apply verticalLook to camera's rotation
+		thisCamera.transform.localEulerAngles = new Vector3(verticalLook,0f,0f);        
+
+        // movement
+
+        fpForwardBackward = Input.GetAxis("Vertical");
+        fpStrafe = Input.GetAxis("Horizontal");
+
+        inputVelocity = transform.forward * fpForwardBackward;
+        inputVelocity += transform.right * fpStrafe;
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+        thisRigidBody.velocity = inputVelocity * velocityModifier + (Physics.gravity * .69f);
+    }
+}
---
+++ Week 7 - Audio/Week 7 - Audio Implementation/Assets/Scripts/raycastKnock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycastKnock : MonoBehaviour
+{
+
+    RaycastHit hit;
+    public AudioSource thisAudioSource;
+    public GameObject hitObject;
+    public AudioClip elevatorMusic;
+
+    void FixedUpdate()
+    {
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 100f))
+        {
+            hitObject = hit.transform.gameObject;
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (hit.transform.gameObject.tag == "Door")
+                {
+                    print("Play sounds");
+                    thisAudioSource.Play();
+                    hitObject.gameObject.GetComponent<AudioSource>().Stop();    // stops sound
+                    hitObject.gameObject.GetComponent<AudioSource>().clip = elevatorMusic;    // replace w elevator music
+                    hitObject.gameObject.GetComponent<AudioSource>().Play();    // play elevator music
+
+                    
+                }
+            }
+        }
+    }
+}
